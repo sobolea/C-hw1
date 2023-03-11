0b688d5117b498c0d3c0c7558f6c3c256d9c887e
67anna <@DESKTOP-VICLL7Q.(none)>
2023-03-11 20:32:12 +0300

    add task4

--- Program.cs
+++ Program.cs
@@ -1,25 +1,39 @@
 ﻿// домашнее задание к семинару 1
 
-/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-a = 5; b = 7 -> max = 7
-a = 2 b = 10 -> max = 10
-a = -9 b = -3 -> max = -3  */
+/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
 
-int a, b, min, max;
+int a, b, c, max;
 Console.Write($"Введите а: ");
 int.TryParse(Console.ReadLine()!, out a);
 Console.Write($"Введите b: ");
 int.TryParse(Console.ReadLine()!, out b);
+Console.Write($"Введите c: ");
+int.TryParse(Console.ReadLine()!, out c);
 
 if (a > b)
 {
-    max = a;
-    min = b;
+    if (a > c)
+    {
+        max = a;
+    }
+    else
+    {
+        max = c;
+    }
 }
 else
 {
-    min = a;
-    max = b;
+    if (b > c)
+    {
+        max = b;
+    }
+    else
+    {
+        max = c;
+    }
 }
 
-Console.Write($"max = {max}, min = {min}");
+Console.Write($"max = {max}");
